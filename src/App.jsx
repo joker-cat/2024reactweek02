57fe377b375d158faccbbe3e576be25edee7aa7f
joker <jocker.ss.cat@gmail.com>
2025-01-05 13:02:31 +0800

    finish week02

--- src/App.jsx
+++ src/App.jsx
@@ -5,75 +5,23 @@ import "./assets/scss/all.scss";
 import "bootstrap";
 
 function App() {
-  const baseUrl = "https://ec-course-api.hexschool.io/";
-  const signInUrl = "v2/admin/signin";
-  const loginCheckUrl = "v2/api/user/check";
-  const [tempProduct, setTempProduct] = useState(null); //存放被選擇的商品
+  const env = import.meta.env;
+  const baseUrl = env.VITE_API_URL;
+  const signInUrl = env.VITE_SIGNIN_URL;
+  const loginCheckUrl = env.VITE_LOGIN_CHECK;
+  const getProductsUrl = env.VITE_GET_PRODUCTS;
+  const [getProducts, setGetProducts] = useState([]);
   const [loginMessage, setLoginMessage] = useState("");
+  const [tempProduct, setTempProduct] = useState(null);
   const [loginStatus, setLoginStatus] = useState(null);
   const [whichButton, setWhichButton] = useState(null);
-  const [products, setProducts] = useState([
-    //假資料
-    {
-      category: "甜甜圈",
-      content: "尺寸：14x14cm",
-      description:
-        "濃郁的草莓風味，中心填入滑順不膩口的卡士達內餡，帶來滿滿幸福感！",
-      id: "-L9tH8jxVb2Ka_DYPwng",
-      is_enabled: 1,
-      origin_price: 150,
-      price: 99,
-      title: "草莓莓果夾心圈",
-      unit: "元",
-      num: 10,
-      imageUrl: "https://images.unsplash.com/photo-1583182332473-b31ba08929c8",
-      imagesUrl: [
-        "https://images.unsplash.com/photo-1626094309830-abbb0c99da4a",
-        "https://images.unsplash.com/photo-1559656914-a30970c1affd",
-      ],
-    },
-    {
-      category: "蛋糕",
-      content: "尺寸：6寸",
-      description:
-        "蜜蜂蜜蛋糕，夾層夾上酸酸甜甜的檸檬餡，清爽可口的滋味讓人口水直流！",
-      id: "-McJ-VvcwfN1_Ye_NtVA",
-      is_enabled: 1,
-      origin_price: 1000,
-      price: 900,
-      title: "蜂蜜檸檬蛋糕",
-      unit: "個",
-      num: 1,
-      imageUrl:
-        "https://images.unsplash.com/photo-1627834377411-8da5f4f09de8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80",
-      imagesUrl: [
-        "https://images.unsplash.com/photo-1618888007540-2bdead974bbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=987&q=80",
-      ],
-    },
-    {
-      category: "蛋糕",
-      content: "尺寸：6寸",
-      description: "法式煎薄餅加上濃郁可可醬，呈現經典的美味及口感。",
-      id: "-McJ-VyqaFlLzUMmpPpm",
-      is_enabled: 1,
-      origin_price: 700,
-      price: 600,
-      title: "暗黑千層",
-      unit: "個",
-      num: 15,
-      imageUrl:
-        "https://images.unsplash.com/photo-1505253149613-112d21d9f6a9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGNha2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
-      imagesUrl: [
-        "https://images.unsplash.com/flagged/photo-1557234985-425e10c9d7f1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA5fHxjYWtlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
-        "https://images.unsplash.com/photo-1540337706094-da10342c93d8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fGNha2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
-      ],
-    },
-  ]);
   const [user, setUser] = useState({
     username: "",
     password: "",
   });
-
+  
+  console.log('我被觸發了');
+  
   useEffect(() => loginCheck, []); // 只在初次渲染時執行
 
   //寫入input
@@ -100,8 +48,8 @@ function App() {
       const token = res.data.token;
       document.cookie = `hexschool=${token}`;
       setLoginMessage(res.data.message);
-
       setLoginStatus(true);
+      getProductsHandler();
     } catch (error) {
       setLoginMessage(error.response.data.message);
       setLoginStatus(false);
@@ -121,10 +69,17 @@ function App() {
       const res = await axios.post(`${baseUrl}${loginCheckUrl}`, {}, config);
       setLoginMessage(res.data.success? '已登入':'未登入');
       setLoginStatus(true);
+      getProductsHandler();
     } catch (error) {
       setLoginMessage(error.response.data.message);
       setLoginStatus(false);
     }
+  }
+
+  //取得產品列表
+  async function getProductsHandler() {
+    const res = await axios.get(`${baseUrl}${getProductsUrl}`);
+    setGetProducts(res.data.products);
   }
 
   return (
@@ -141,6 +96,7 @@ function App() {
               className="htmlForm-control-plaintext"
               id="staticEmail"
               onChange={inputHandler}
+              disabled={loginStatus}
             />
           </div>
         </div>
@@ -158,6 +114,7 @@ function App() {
               className="htmlForm-control"
               id="inputPassword"
               onChange={inputHandler}
+              disabled={loginStatus}
             />
           </div>
         </div>
@@ -230,7 +187,7 @@ function App() {
                     </tr>
                   </thead>
                   <tbody>
-                    {products.map((item, index) => (
+                    {getProducts.map((item, index) => (
                       <tr key={index}>
                         <td>{item.category}</td>
                         <td>{item.origin_price}</td>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react' //嚴格模式引用
 import { createRoot } from 'react-dom/client'
 import './assets/scss/all.scss'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode> //嚴格模式
     <App />
-  </StrictMode>,
+  // </StrictMode>,
 )
+
+/*
+  在 React 18 中，React 的嚴格模式（Strict Mode）
+  會導致某些函數在開發模式下被調用兩次，
+  以幫助開發者識別和修復潛在的問題。
+  這可能是你看到 console.log(baseUrl) 重複印出的原因。
+
+  嚴格模式的這種行為只會在開發模式下發生，
+  不會影響生產環境。
+  你可以通過檢查你的 React 應用是否在嚴格模式下運行來確認這一點。
+
+  以下是如何檢查和移除嚴格模式：
+
+  檢查 index.jsx 或 index.js 文件： 
+  檢查你的應用是否在嚴格模式下運行。
+  通常，這是在 index.jsx 或 index.js 文件中設置的。
+*/ 
